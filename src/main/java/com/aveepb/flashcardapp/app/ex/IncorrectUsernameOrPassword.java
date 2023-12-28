14ab3f224c739bebfc2bbbee98ea74d969ca374e
AveePB <mike303.py@gmail.com>
2023-12-28 14:25:04 +0100

    Created custom exceptions

---
+++ src/main/java/com/aveepb/flashcardapp/app/ex/IncorrectUsernameOrPassword.java
@@ -0,0 +1,5 @@
+package com.aveepb.flashcardapp.app.ex;
+
+public class IncorrectUsernameOrPassword extends Exception {
+
+}
---
+++ src/main/java/com/aveepb/flashcardapp/app/ex/UsernameAlreadyTaken.java
@@ -0,0 +1,5 @@
+package com.aveepb.flashcardapp.app.ex;
+
+public class UsernameAlreadyTaken extends Exception {
+
+}
