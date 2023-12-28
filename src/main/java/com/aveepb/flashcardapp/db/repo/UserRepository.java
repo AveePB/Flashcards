e4a03f2828e47cfcfe88b16f2bbb01ef29f81fdd
AveePB <mike303.py@gmail.com>
2023-12-28 14:24:42 +0100

    Updated db package

--- src/main/java/com/aveepb/flashcardapp/db/repo/UserRepository.java
+++ src/main/java/com/aveepb/flashcardapp/db/repo/UserRepository.java
@@ -5,7 +5,12 @@ import com.aveepb.flashcardapp.db.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    //READ:
+    Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
