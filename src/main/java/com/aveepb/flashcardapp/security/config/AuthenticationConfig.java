06f4cedc442d42e35f21573f5473b395d9d8f7fc
AveePB <mike303.py@gmail.com>
2023-12-30 11:40:58 +0100

    Updated security package

--- src/main/java/com/aveepb/flashcardapp/security/config/AuthenticationConfig.java
+++ src/main/java/com/aveepb/flashcardapp/security/config/AuthenticationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class AuthConfig {
+public class AuthenticationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
