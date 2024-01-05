fa3bdb8e18f1f5d033f0ef793d2b87b1d1fa80b6
AveePB <mike303.py@gmail.com>
2024-01-05 18:33:45 +0100

    Updated configs

--- src/main/java/com/aveepb/flashcardapp/security/config/AuthConfig.java
+++ src/main/java/com/aveepb/flashcardapp/security/config/AuthConfig.java
@@ -1,14 +1,40 @@
 package com.aveepb.flashcardapp.security.config;
 
+import com.aveepb.flashcardapp.web.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class AuthConfig {
+
+    private final UserService userService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider dao = new DaoAuthenticationProvider();
+        dao.setUserDetailsService(this.userService);
+        dao.setPasswordEncoder(passwordEncoder);
+
+        return dao;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+
+        return config.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/aveepb/flashcardapp/security/config/SecurityConfig.java
+++ src/main/java/com/aveepb/flashcardapp/security/config/SecurityConfig.java
@@ -1,4 +1,33 @@
 package com.aveepb.flashcardapp.security.config;
 
-public class SecurityConfig {s
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationProvider provider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/*")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                );
+
+        return httpSecurity.build();
+    }
 }
