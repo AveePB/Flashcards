636dc90a2e69c0d5cd7a76bc985f1250c05257b6
AveePB <mike303.py@gmail.com>
2024-01-20 15:19:25 +0100

    Updated the flashcard dto mapper

--- src/main/java/dev/bpeeva/flashcardsapp/util/mapper/FlashcardDTOMapper.java
+++ src/main/java/dev/bpeeva/flashcardsapp/util/mapper/FlashcardDTOMapper.java
@@ -3,16 +3,25 @@ package dev.bpeeva.flashcardsapp.util.mapper;
 import dev.bpeeva.flashcardsapp.db.model.Flashcard;
 import dev.bpeeva.flashcardsapp.util.dto.FlashcardDTO;
 
+import java.util.Optional;
 import java.util.function.Function;
 
-public class FlashcardDTOMapper implements Function<Flashcard, FlashcardDTO> {
+public class FlashcardDTOMapper implements Function<Flashcard, Optional<FlashcardDTO>> {
 
     @Override
-    public FlashcardDTO apply(Flashcard flashcard) {
-        return new FlashcardDTO(
-                flashcard.getWordType(),
-                flashcard.getEnglishWord(),
-                flashcard.getPolishWord()
-        );
+    public Optional<FlashcardDTO> apply(Flashcard flashcard) {
+        //Check if not null.
+        if (flashcard.getWordType() == null ||
+                flashcard.getEnglishWord() == null ||
+                flashcard.getPolishWord() == null)
+            return Optional.empty();
+
+        //Create new object.
+        return Optional.of(
+                new FlashcardDTO(
+                        flashcard.getWordType(),
+                        flashcard.getEnglishWord(),
+                        flashcard.getPolishWord()
+                ));
     }
 }
