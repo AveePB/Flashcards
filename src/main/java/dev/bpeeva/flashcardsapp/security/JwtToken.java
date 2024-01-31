2b0dee321bfadce0c8a062551082a43121564318
AveePB <mike303.py@gmail.com>
2024-01-31 14:38:12 +0100

    Created JWT token class

---
+++ src/main/java/dev/bpeeva/flashcardsapp/security/JwtToken.java
@@ -0,0 +1,15 @@
+package dev.bpeeva.flashcardsapp.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtToken {
+
+    private String bearer;
+}
