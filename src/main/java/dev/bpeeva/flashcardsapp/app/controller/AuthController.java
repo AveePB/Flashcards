8a76eb46c132f22fb246a418329d8dbe49fdd3eb
AveePB <mike303.py@gmail.com>
2024-01-31 14:38:29 +0100

    Created auth controller

---
+++ src/main/java/dev/bpeeva/flashcardsapp/app/controller/AuthController.java
@@ -0,0 +1,50 @@
+package dev.bpeeva.flashcardsapp.app.controller;
+
+import dev.bpeeva.flashcardsapp.app.service.UserService;
+import dev.bpeeva.flashcardsapp.db.model.User;
+import dev.bpeeva.flashcardsapp.security.JwtToken;
+import dev.bpeeva.flashcardsapp.util.dto.UserDTO;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<JwtToken> signUp(@RequestBody UserDTO userDTO) {
+        //Create new user.
+        Optional<User> user = this.userService.createUser(userDTO);
+
+        //If operation successful.
+        if (user.isPresent())
+            return ResponseEntity.status(HttpStatus.OK).body(new JwtToken("NO BEARER TOKEN"));
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtToken> logIn(@RequestBody UserDTO userDTO) {
+        //Check credentials.
+        Optional<User> user = this.userService.getUser(userDTO);
+
+        //If operation successful.
+        if (user.isPresent())
+            return ResponseEntity.status(HttpStatus.OK).body(new JwtToken("NO BEARER TOKEN"));
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+    //PUT VALUE INTO JWT TOKEN CONSTRUCTOR!!!!!
+}
