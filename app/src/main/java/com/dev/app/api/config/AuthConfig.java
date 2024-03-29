b4af9e222b5e4cc577b18b96481795662047f17b
AveePB <mike303.py@gmail.com>
2024-03-29 22:22:10 +0100

    Updated auth config

--- app/src/main/java/com/dev/app/api/config/AuthConfig.java
+++ app/src/main/java/com/dev/app/api/config/AuthConfig.java
@@ -1,7 +1,9 @@
 package com.dev.app.api.config;
 
 import com.dev.app.bll.manager.UserManager;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
