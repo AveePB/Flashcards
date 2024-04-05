55a06d88c05969264c0bd9c618c81260bc1947dc
AveePB <mike303.py@gmail.com>
2024-04-05 22:01:59 +0200

    Updated the filter chain

--- app/src/main/java/com/dev/app/api/config/SecurityConfig.java
+++ app/src/main/java/com/dev/app/api/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dev.app.api.config;
 
+import com.dev.app.api.filter.JWTFilter;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,16 +21,19 @@ public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
 
+    private final JWTFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/*")
+                        .requestMatchers("/auth/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
                 )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider);
 
